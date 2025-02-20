daf4648185dfa462a7ae876a36c000264e7fd882
IMPraveenRaj <impraveen13@gmail.com>
2025-02-20 23:49:43 +0100

    have added small changes for the streams

--- src/main/java/com/springheaven/transformx/stream/StreamEx.java
+++ src/main/java/com/springheaven/transformx/stream/StreamEx.java
@@ -1,4 +1,13 @@
 package com.springheaven.transformx.stream;
 
+import java.util.List;
+
 public class StreamEx {
+
+    public static void main(String[] args) {
+        List<String> emailList = List.of("iampraveen13@gmail.com", "impraveenraj13@gmail.com",
+                "dharshini.jc@gmail.com", "vasanthi@gmail.com");
+
+        emailList.stream().filter(emails-> emails.contains("raj")).forEach(System.out::println);
+    }
 }
